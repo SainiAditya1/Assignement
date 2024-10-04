b181c5c33a2f513555c1def89bb6a9295bad45fc
Aditya Saini <adityasaini10012001@outlook.com>
2024-10-04 12:22:35 +0530

    minor fixes
    Signed-off-by: Aditya Saini <adityasaini10012001@outlook.com>

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,7 +1,9 @@
 const Home = () => {
   return (
-    <div className="w-full h-screen flex justify-center bg-[#f3f8c5] pt-20">
-      <p className="font-bold text-lg">Go to /dashboard to view the Dashboard</p>
+    <div className="w-full h-screen flex justify-center bg-[hsl(136,78%,87%)] pt-20">
+      <p className="font-bold text-lg">Go to /dashboard to view the Dashboard </p>
+
+      <p className="font-bold text-lg"></p>
     </div>
   );
 };
